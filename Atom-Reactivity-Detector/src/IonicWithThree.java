a4cf9a4587038797d61604c6a656a5e3dcd7a6d4
Carter Weiss <benanddana92@gmail.com>
2015-11-04 11:19:39 -0800

    added ionicwiththree class

---
+++ Atom-Reactivity-Detector/src/IonicWithThree.java
@@ -0,0 +1,14 @@
+
+public class IonicWithThree extends IonicResultFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 988536625939754581L;
+
+	protected IonicWithThree(Atom[] atoms) {
+		super(atoms);
+		// TODO Auto-generated constructor stub
+	}
+
+}
