ef065791aa108b1e28f12c7c8884c71053ab4d60
Carter <benanddnaa92@gmail.com>
2015-11-05 18:26:44 -0800

    THIS WILL SHOW ATOMS - IF NO WORKING COPY USE THIS

--- Atom-Reactivity-Detector/src/IonicResultFrame.java
+++ Atom-Reactivity-Detector/src/IonicResultFrame.java
@@ -14,7 +14,7 @@ public class IonicResultFrame extends JFrame {
 	protected int ySize;
 	protected int frame = 0;
 	protected boolean go;
-	private ActionListener al = new ActionListener() {
+	protected ActionListener al = new ActionListener() {
 
 		@Override
 		public void actionPerformed(ActionEvent arg0) {
@@ -31,6 +31,7 @@ public class IonicResultFrame extends JFrame {
 		System.out.println("made it");
 		setVisible(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setBackground(Color.BLACK);
 		go = true;
 		setSize(new Dimension(10, 10));
 		System.out.println(convertAtomToResultAtom(atoms[0], Resources.IONIC, 0).getName());
@@ -66,7 +67,6 @@ public class IonicResultFrame extends JFrame {
 
 	@Override
 	public void paint(Graphics g) {
-		setBackground(Color.BLACK);
 		Graphics2D g2D = (Graphics2D) g;
 		if (frame > 15) {
 			xSize += 10;
@@ -82,12 +82,12 @@ public class IonicResultFrame extends JFrame {
 		if (xSize >= xMax && ySize >= yMax) {
 			// ???
 			if (frame < xMax / 10 + xMax / 16)
-				if (frame < xMax / 10 && atoms[0].getX() < xMax / 10 * 4) {
-					atoms[0].setX(atoms[0].getX() + 4);
-					atoms[1].setX(atoms[1].getX() - 4);
+				if (frame < xMax / 10 && atoms[0].getX() < xMax / 8 * 7) {
+					atoms[0].setX(atoms[0].getX() + 2);
+					atoms[1].setX(atoms[1].getX() - 2);
 				}
 			if (go) {
-				leftCurrentPosition[0] += 3;
+				leftCurrentPosition[0] += 2;
 			}
 			System.out.println(leftCurrentPosition[0] + " dfdfdfdfd " + (atoms[1].getX() + Resources.electronPos[4][0]));
 			if (leftCurrentPosition[0] > atoms[1].getX() - 125) {
@@ -100,7 +100,7 @@ public class IonicResultFrame extends JFrame {
 			g2D.drawString(atoms[0].getName(), atoms[0].getX(), atoms[0].getY());
 			g2D.setColor(Color.BLUE);
 			g2D.drawString(atoms[1].getName(), atoms[1].getX(), atoms[1].getY());
-			g2D.setColor(Color.YELLOW);
+			g2D.setColor(Color.GREEN);
 			for (int i = 0; i < atoms[0].getValenceElectrons(); i++) {
 				for (int i2 = 0; i2 < atoms[0].v.length; i2++) {
 					if (atoms[0].v[i2]) {
@@ -109,7 +109,7 @@ public class IonicResultFrame extends JFrame {
 					}
 				}
 			}
-			g2D.setColor(Color.GREEN);
+			g2D.setColor(Color.YELLOW);
 			for (int i = 0; i < atoms[1].getValenceElectrons(); i++) {
 				for (int i2 = 0; i2 < atoms[1].v.length; i2++) {
 					if (atoms[1].v[i2]) {
--- Atom-Reactivity-Detector/src/IonicWithThree.java
+++ Atom-Reactivity-Detector/src/IonicWithThree.java
@@ -1,4 +1,8 @@
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.Timer;
 
 public class IonicWithThree extends IonicResultFrame {
 
@@ -11,14 +15,26 @@ public class IonicWithThree extends IonicResultFrame {
 	private ResultAtom middleOne;
 	protected IonicWithThree(Atom middleOne, Atom[] others) {
 		super(others);
-		rightCurrentPosition[0] = xMax / 4 * 3 + Resources.electronPos[5][0];
 		this.others[0] = convertAtomToResultAtom(others[0], Resources.IONIC3, 1);
 		this.others[1] = convertAtomToResultAtom(others[1], Resources.IONIC3, 0);
 		this.middleOne = convertAtomToResultAtom(middleOne, Resources.IONIC3, 2);
-	}
-
-	public void paint(Graphics g) {
 		setBackground(Color.BLACK);
+		rightCurrentPosition[0] = xMax / 2 + 300;
+		leftCurrentPosition[1] = xMax / 2 - 300;
+ 	}
+	protected ActionListener al = new ActionListener() {
+
+		@Override
+		public void actionPerformed(ActionEvent arg0) {
+			frame++;
+			System.out.println(frame);
+			repaint();
+		}
+	};
+	protected Timer t = new Timer(10, al);
+
+	@Override
+	public void paint(Graphics g) {
 		t.start();
 		Graphics2D g2D = (Graphics2D) g;
 		if (frame > 15) {
@@ -34,15 +50,13 @@ public class IonicWithThree extends IonicResultFrame {
 		}
 		if (xSize >= xMax && ySize >= yMax) {
 			// ???
-			if (frame < xMax / 10 + xMax / 16) {
-				if (frame < xMax / 10 && others[0].getX() < xMax / 10 * 4) {
-					others[0].setX(others[0].getX() + 4);
-					others[1].setX(others[1].getX() - 4);
+			if (frame < xMax / 10 + xMax / 16)
+				if (frame < xMax / 10 && atoms[0].getX() < xMax / 8 * 7) {
+					atoms[0].setX(atoms[0].getX() + 2);
+					atoms[1].setX(atoms[1].getX() - 2);
 				}
-			}
 			if (go) {
-				leftCurrentPosition[0] += 3;
-				rightCurrentPosition[0] -= 3;
+				leftCurrentPosition[0] += 2;
 			}
 			System.out.println(leftCurrentPosition[0] + " dfdfdfdfd " + (others[1].getX() + Resources.electronPos[4][0]));
 			if (leftCurrentPosition[0] >  - 125) {
@@ -50,36 +64,37 @@ public class IonicWithThree extends IonicResultFrame {
 				go = false;
 			}
 			g2D.clearRect(0, 0, xMax, yMax);
+			g2D.setColor(Color.WHITE);
+			g2D.drawString(middleOne.getName(), xMax / 2, yMax / 2);
 			g2D.setColor(Color.RED);
 			System.out.println();
-			g2D.drawString(others[0].getName(), others[0].getX(), others[0].getY());
+			g2D.drawString(atoms[0].getName(), atoms[0].getX(), atoms[0].getY());
 			g2D.setColor(Color.BLUE);
-			g2D.drawString(others[1].getName(), others[1].getX(), others[1].getY());
-			g2D.drawString(middleOne.getName(), xMax / 2, yMax / 2);
-			g2D.setColor(Color.YELLOW);
-			for (int i = 0; i < others[0].getValenceElectrons(); i++) {
-				for (int i2 = 0; i2 < 8; i2++) {
-					if (others[0].v[i2]) {
+			g2D.drawString(atoms[1].getName(), atoms[1].getX(), atoms[1].getY());
+			g2D.setColor(Color.GREEN);
+			for (int i = 0; i < atoms[0].getValenceElectrons(); i++) {
+				for (int i2 = 0; i2 < atoms[0].v.length; i2++) {
+					if (atoms[0].v[i2]) {
 						g2D.fillOval(leftCurrentPosition[0] + Resources.electronPos[i][0],
-								others[0].getY() + Resources.electronPos[i][1], 5, 5);
+								atoms[0].getY() + Resources.electronPos[i][1], 5, 5);
 					}
 				}
 			}
-			g2D.setColor(Color.GREEN);
-			for (int i = 0; i < others[1].getValenceElectrons(); i++) {
-				for (int i2 = 0; i2 < 8; i2++) {
-					if (others[1].v[i2]) {
-						g2D.fillOval(rightCurrentPosition[0] + Resources.electronPos[i][0],
-								others[0].getY() + Resources.electronPos[i][1], 5, 5);
+			g2D.setColor(Color.YELLOW);
+			for (int i = 0; i < atoms[1].getValenceElectrons(); i++) {
+				for (int i2 = 0; i2 < atoms[1].v.length; i2++) {
+					if (atoms[1].v[i2]) {
+						g2D.fillOval(atoms[1].getX() + Resources.electronPos[i][0],
+								atoms[0].getY() + Resources.electronPos[i][1], 5, 5);
 					}
 				}
 			}
-			g2D.setColor(Color.GREEN);
+			g2D.setColor(Color.WHITE);
 			for (int i = 0; i < middleOne.getValenceElectrons(); i++) {
 				for (int i2 = 0; i2 < 8; i2++) {
 					if (others[1].v[i2]) {
 						g2D.fillOval(xMax / 2 + Resources.electronPos[i][0],
-								xMax / 2 + Resources.electronPos[i][1], 5, 5);
+								yMax / 2 + Resources.electronPos[i][1], 5, 5);
 					}
 				}
 			}
